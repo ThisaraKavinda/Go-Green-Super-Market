911c8a1b27df2c0e94c50de1f7d59ffabef92227
ThisaraKavinda <thisara48kavinda@gmail.com>
2023-04-07 15:46:06 +0530

    feat: initialized backend

---
+++ backend/models/product.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  type: {
+    required: true,
+    type: String,
+  },
+  category: {
+    required: true,
+    type: String,
+  },
+  price: {
+    required: true,
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  availability: {
+    required: true,
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+});
+
+export const Product = mongoose.model("products", ProductSchema);
